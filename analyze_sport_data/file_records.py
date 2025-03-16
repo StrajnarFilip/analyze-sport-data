1227bbef7fc0f00a0d3fcebdf1b99ed89c8b7363
Filip <filip.strajnar@gmail.com>
2025-03-16 10:51:25 +0100

    Adding heart rate offset to heart rate records.

--- analyze_sport_data/file_records.py
+++ analyze_sport_data/file_records.py
@@ -9,5 +9,8 @@ class FileRecords:
                  heart_rate_records: list[HeartRateRecord],
                  heart_rate_time_offset: timedelta = timedelta()):
         self.name = name
-        self.heart_rate_records = heart_rate_records
+        self.heart_rate_records = [
+            HeartRateRecord((record.recorded_at + heart_rate_time_offset),
+                            record.heart_rate) for record in heart_rate_records
+        ]
         self.heart_rate_time_offset = heart_rate_time_offset
